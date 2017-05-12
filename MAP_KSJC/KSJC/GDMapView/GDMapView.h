56861ed79d3d153011c8e879323b96023012db8c
shiyong_li <1525846137@qq.com>
2017-05-12 17:30:54 +0800

    add file for GDMapView

---
+++ MAP_KSJC/KSJC/GDMapView/GDMapView.h
@@ -0,0 +1,13 @@
+//
+//  GDMapView.h
+//  KSJC
+//
+//  Created by shiyong_li on 2017/5/12.
+//  Copyright © 2017年 dist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GDMapView : UIView
+
+@end
---
+++ MAP_KSJC/KSJC/GDMapView/GDMapView.m
@@ -0,0 +1,21 @@
+//
+//  GDMapView.m
+//  KSJC
+//
+//  Created by shiyong_li on 2017/5/12.
+//  Copyright © 2017年 dist. All rights reserved.
+//
+
+#import "GDMapView.h"
+
+@implementation GDMapView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
